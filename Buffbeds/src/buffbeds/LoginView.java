9017cb8d5470fbc2a1b6045217aea3da097fb484
Sarang Kulkarni <sarang.kulkarni@colorado.edu>
2017-12-10 16:16:15 -0700

    Adding database connection to loginview

--- Buffbeds/src/buffbeds/LoginView.java
+++ Buffbeds/src/buffbeds/LoginView.java
@@ -2,17 +2,22 @@ package buffbeds;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Image;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.awt.event.ActionEvent;
 import javax.swing.JPasswordField;
+import java.sql.*;
 
 public class LoginView extends JFrame {
 
@@ -23,27 +28,29 @@ public class LoginView extends JFrame {
 	private final JLabel lblUsername = new JLabel("Username:");
 	private final JLabel lblPassword = new JLabel("Password:");
 	private final JPasswordField textPassword = new JPasswordField();
+	private final JLabel imagelbl = new JLabel("");
 
 	/**
 	 * Launch the application.
 	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					LoginView frame = new LoginView();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginView frame = new LoginView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	Connection connection = null;
+
 	public LoginView() {
 		textUsername.setBounds(197, 67, 99, 20);
 		textUsername.setColumns(10);
@@ -63,9 +70,37 @@ public class LoginView extends JFrame {
 		contentPane.add(textUsername);
 		LoginBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				AdminView admin_view = new AdminView();
-				setVisible(false);
-				admin_view.setVisible(true);
+				try{
+					String sql_query = "Select * from UserInfo where username=? and password=?";
+					PreparedStatement prep_state = connection.prepareStatement(sql_query);
+					prep_state.setString(1, textUsername.getText());
+					prep_state.setString(2, textPassword.getText());
+					ResultSet res_set = prep_state.executeQuery();
+					
+					if(res_set.next()) {
+						//JOptionPane.showMessageDialog(null, res_set.getString("AccessType"));
+						if(res_set.getString("AccessType").equals("Admin")) {
+						//see to open which view
+							AdminView admin_view = new AdminView();
+							dispose();
+							admin_view.setVisible(true);
+						}
+						else if(res_set.getString("AccessType").equals("Customer")) {
+							UserView user_view = new UserView();
+							dispose();
+							user_view.setVisible(true);
+						}
+						else if(res_set.getString("AccessType").equals("Host")) {
+							//do something for host
+						}
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "Wrong Password");
+					}
+				}catch(Exception ex) {
+					JOptionPane.showMessageDialog(null, "cant open");
+				}
+				
 			}
 		});
 		LoginBtn.setBounds(93, 129, 89, 20);
@@ -90,5 +125,11 @@ public class LoginView extends JFrame {
 		textPassword.setBounds(197, 98, 99, 20);
 		
 		contentPane.add(textPassword);
+		imagelbl.setBounds(24, 11, 75, 60);
+		
+		contentPane.add(imagelbl);
+		
+		Image ralphie = new ImageIcon(this.getClass().getResource("/ralphie.png")).getImage();
+		imagelbl.setIcon(new ImageIcon("C:\\Users\\Sarang\\eclipse-workspace\\Buffbeds\\bin\\ralphie.png"));
 	}
 }
