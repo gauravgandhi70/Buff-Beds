a752eb743ff131d1b22e6a87b032176bf80ca255
gauravgandhi70 <gaurav.gandhi@colorado.edu>
2017-12-11 18:28:59 -0700

    Modified Newbooking class

--- Buffbeds/src/buffbeds/NewBooking.java
+++ Buffbeds/src/buffbeds/NewBooking.java
@@ -1,9 +1,50 @@
 package buffbeds;
 
+import buffbeds.Filter;
+
 public class NewBooking {
 
-	public NewBooking() {
-		// Default constructor stub
+	private Filter filtercriteria;
+	private Boolean extraBed;
+	private Boolean food;
+	private float Total;
+	
+	public NewBooking()
+	{
+		
 	}
 
+	public Filter getFiltercriteria() {
+		return filtercriteria;
+	}
+
+	public void setFiltercriteria(Filter filtercriteria) {
+		this.filtercriteria = filtercriteria;
+	}
+
+	public Boolean getExtraBed() {
+		return extraBed;
+	}
+
+	public void setExtraBed(Boolean extraBed) {
+		this.extraBed = extraBed;
+	}
+
+	public Boolean getFood() {
+		return food;
+	}
+
+	public void setFood(Boolean food) {
+		this.food = food;
+	}
+
+	public float getTotal() {
+		return Total;
+	}
+
+	public void setTotal(float total) {
+		Total = total;
+	}
+	
+	
 }
