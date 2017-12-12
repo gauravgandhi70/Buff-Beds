190c8077573b93bfab62ce5c778a0afe330ae799
gauravgandhi70 <gaurav.gandhi@colorado.edu>
2017-12-11 18:29:58 -0700

    Modified Request class

--- Buffbeds/src/buffbeds/Request.java
+++ Buffbeds/src/buffbeds/Request.java
@@ -1,9 +1,49 @@
 package buffbeds;
 
 public class Request {
-
-	public Request() {
-		// Default constructor stub
+	private String Type;
+	private int ReqID;
+	private int RequestedID;
+	private int responderID;
+	
+	public Request()
+	{
+		
 	}
+
+	public String getType() {
+		return Type;
+	}
+
+	public void setType(String type) {
+		Type = type;
+	}
+
+	public int getReqID() {
+		return ReqID;
+	}
+
+	public void setReqID(int reqID) {
+		ReqID = reqID;
+	}
+
+	public int getRequestedID() {
+		return RequestedID;
+	}
+
+	public void setRequestedID(int requestedID) {
+		RequestedID = requestedID;
+	}
+
+	public int getResponderID() {
+		return responderID;
+	}
+
+	public void setResponderID(int responderID) {
+		this.responderID = responderID;
+	}
+	
+	
+	
 
 }
