5ca27ac6051bb9c6b13cfd5a5ce9ae19a4f71601
gauravgandhi70 <gaurav.gandhi@colorado.edu>
2017-12-11 18:29:33 -0700

    Modified Payment class

--- Buffbeds/src/buffbeds/Payment.java
+++ Buffbeds/src/buffbeds/Payment.java
@@ -1,9 +1,46 @@
 package buffbeds;
 
 public class Payment {
+	private String cardNo;
+	private String expiryDate;
+	private int CVV;
+	private float Total;
+	
+	Payment()
+	{
+		
+	}
 
-	public Payment() {
-		// Default constructor stub
+	public String getCardNo() {
+		return cardNo;
+	}
+
+	public void setCardNo(String cardNo) {
+		this.cardNo = cardNo;
+	}
+
+	public String getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(String expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+
+	public int getCVV() {
+		return CVV;
+	}
+
+	public void setCVV(int cVV) {
+		CVV = cVV;
+	}
+
+	public float getTotal() {
+		return Total;
+	}
+
+	public void setTotal(float total) {
+		Total = total;
 	}
 
 }
